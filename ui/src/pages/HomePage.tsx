eda8aaf5c44a1fc6185d9b03f32d0c56dff91328
himok <himok.pk@gmail.com>
2023-03-04 21:47:52 +0100

    Added all category in UI

--- ui/src/pages/HomePage.tsx
+++ ui/src/pages/HomePage.tsx
@@ -26,8 +26,13 @@ const HomePage: React.FC = () => {
     }, []);
 
     useEffect(() => {
+        const allCategory: Category = {
+            id: 0,
+            name: "All",
+            svg_url: "0",
+        };
         getData({ url: "/api/category/list/" }).then(response => {
-            setCategories(response);
+            setCategories(() => [allCategory, ...response]);
         });
     }, []);
 
@@ -48,6 +53,36 @@ const HomePage: React.FC = () => {
         setCategory(Number(id));
     };
 
+    useEffect(() => {
+        const slider = document.querySelector(".categories-container") as HTMLDivElement;
+        let isDown = false;
+        let startX: number;
+        let scrollLeft: number;
+
+        slider.addEventListener("mousedown", e => {
+            isDown = true;
+            slider.classList.add("active");
+            startX = e.pageX - slider.offsetLeft;
+            scrollLeft = slider.scrollLeft;
+        });
+        slider.addEventListener("mouseleave", () => {
+            isDown = false;
+            slider.classList.remove("active");
+        });
+        slider.addEventListener("mouseup", () => {
+            isDown = false;
+            slider.classList.remove("active");
+        });
+        slider.addEventListener("mousemove", e => {
+            if (!isDown) return;
+            e.preventDefault();
+            const x = e.pageX - slider.offsetLeft;
+            const walk = (x - startX) * 3; //scroll-fast
+            slider.scrollLeft = scrollLeft - walk;
+            console.log(walk);
+        });
+    }, []);
+
     return (
         <div className="container">
             <NavBar />
@@ -55,6 +90,17 @@ const HomePage: React.FC = () => {
                 <MdOutlineKeyboardArrowLeft id="left-arrow" />
                 <div className="categories-container">
                     {categories.map((object: Category) => {
+                        if (object.name === "All") {
+                            return (
+                                <div
+                                    className="category prevent-select"
+                                    id={"category-" + object.id}
+                                    onClick={changeCategory}
+                                >
+                                    <span>{object.name}</span>
+                                </div>
+                            );
+                        }
                         return (
                             <div
                                 className="category prevent-select"
